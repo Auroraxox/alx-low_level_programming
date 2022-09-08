b2fe0a7fdc4819e4adddf8c304307e9ea8b25784
Auroraxox <wonuolaademola@gmail.com>
2022-09-08 16:56:26 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - entry point ; void - no input arguments
+ * description - print "\Programming is like building a multilingual puzzle
+ * \n - newline
+ * return - 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
