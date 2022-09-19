2ef2da75c839d65d2b80fab4650072f6a867acae
Auroraxox <wonuolaademola@gmail.com>
2022-09-19 11:28:34 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - update pointer value to 98
+ * @n: integer pointer to the variable
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98
+}
