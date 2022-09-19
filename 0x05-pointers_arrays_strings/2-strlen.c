90f7ada236f80f23b96aa07d2cb920e3ba0b9a03
Auroraxox <wonuolaademola@gmail.com>
2022-09-19 12:38:05 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to return length
+ * Return: int length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+	length++;
+	return (length);
+}
